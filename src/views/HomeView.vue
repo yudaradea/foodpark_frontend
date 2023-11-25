a7b589a8c3700cce4374fc6a83cab77da2f631a4
Yuda Aradea <yudaaradeagit@gmail.com>
2023-11-25 23:58:53 +0700

    Bisa Yuk

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,10 @@ import OverItem from "../components/home/OverItem.vue";
 
 import { loadScript } from "vue-plugin-load-script";
 
-await loadScript("./public/js/wow.min.js").then(() => {
+loadScript("../../public/js/wow.min.js").then(() => {
   new WOW().init();
 });
-await loadScript("./public/js/Font-Awesome.js");
+loadScript("../../public/js/Font-Awesome.js");
 </script>
 
 <template>
