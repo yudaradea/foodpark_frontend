fad17d5601c5fb7c2f51574d738059e6cbcecd64
Yuda Aradea <yudaaradeagit@gmail.com>
2023-11-25 23:51:22 +0700

    Add ./

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,10 +5,10 @@ import OverItem from "../components/home/OverItem.vue";
 
 import { loadScript } from "vue-plugin-load-script";
 
-await loadScript("/public/js/wow.min.js").then(() => {
+await loadScript("./public/js/wow.min.js").then(() => {
   new WOW().init();
 });
-await loadScript("/public/js/Font-Awesome.js");
+await loadScript("./public/js/Font-Awesome.js");
 </script>
 
 <template>
